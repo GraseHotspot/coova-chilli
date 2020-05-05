2d45d0703850d9279a5c5a6cb83bac68a6138b90
Tim White <tim@whiteitsolutions.com.au>
2020-05-05 21:25:37 +0800

    We no longer use squid by default so don't redirect to it

--- debian/extra_files/ipup.sh
+++ debian/extra_files/ipup.sh
@@ -5,8 +5,9 @@
 #    ipt -I PREROUTING -t nat -i $IF -p tcp -s 10.1.0.0/24 -d ! 10.1.0.1 --dport 80 -j REDIRECT --to 8080
 
     # Redirect to Squid proxy (drop direct attempts to proxy)
-    ipt -I PREROUTING -t mangle -p tcp -s $NET/$MASK -d $ADDR --dport 3128 -j DROP
-    ipt -I PREROUTING -t nat -i $TUNTAP -p tcp -s $NET/$MASK ! -d $ADDR --dport 80 -j REDIRECT --to 3128
+    # ipt -I PREROUTING -t mangle -p tcp -s $NET/$MASK -d $ADDR --dport 3128 -j DROP
+    # ipt -I PREROUTING -t nat -i $TUNTAP -p tcp -s $NET/$MASK ! -d $ADDR --dport 80 -j REDIRECT --to 3128
+
     # Look at using this rule?
     # ipt -I PREROUTING  -t nat -i $TUNTAP -p tcp -s $NET/$MASK -d ! $ADDR --dport 80 -j DNAT --to 192.168.8.22:3128
     
